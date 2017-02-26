df7946bed55412e9e3b1f47f1894de2bc60dfef9
Francesco Bellentani <francesco.bellentani@gmail.com>
2017-02-26 18:35:37 +0100

    Removed arquillian

--- src/test/java/bbarena/server/TestServer.java
+++ src/test/java/bbarena/server/TestServer.java
@@ -19,46 +19,46 @@ import java.net.URL;
 /**
  * Created by teg on 18/12/16.
  */
-@RunWith(Arquillian.class)
-public class TestServer {
-
-    private static final Logger _logger = LoggerFactory.getLogger(TestServer.class);
-
-    @Deployment
-    public static WebArchive createDeployment() {
-        // Import Maven runtime dependencies
-        File[] files = Maven.resolver().resolve("bbarena:bbarena:1.0-SNAPSHOT")
-                .withTransitivity().asFile();
-
-        // Create deploy file
-        WebArchive war = ShrinkWrap.create(WebArchive.class)
-//                .addPackage("bbarena.server")
-                .addAsLibraries(files);
-//        war.merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class)
-//                        .importDirectory("src/main/webapp").as(GenericArchive.class),
-//                "/", Filters.includeAll());
-
-        // Show the deploy structure
-        _logger.info(war.toString(true));
-
-        return war;
-    }
-
-    @Test
-    public void testServer(@ArquillianResource URL url) throws InterruptedException, URISyntaxException {
-        // open websocket
-        URI endpointURI = new URI("ws://" + url.getHost() + ":" + url.getPort() + url.getPath() + "match/m/c");
-        _logger.info(endpointURI.toString());
-        final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(endpointURI);
-
-        // add listener
-        clientEndPoint.addMessageHandler(message -> System.out.println(message));
-
-        // send message to websocket
-        clientEndPoint.sendMessage("start match");
-
-        // wait 5 seconds for messages from websocket
-        Thread.sleep(1000);
-
-    }
-}
+//@RunWith(Arquillian.class)
+//public class TestServer {
+//
+//    private static final Logger _logger = LoggerFactory.getLogger(TestServer.class);
+//
+//    @Deployment
+//    public static WebArchive createDeployment() {
+//        // Import Maven runtime dependencies
+//        File[] files = Maven.resolver().resolve("bbarena:bbarena:1.0-SNAPSHOT")
+//                .withTransitivity().asFile();
+//
+//        // Create deploy file
+//        WebArchive war = ShrinkWrap.create(WebArchive.class)
+////                .addPackage("bbarena.server")
+//                .addAsLibraries(files);
+////        war.merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class)
+////                        .importDirectory("src/main/webapp").as(GenericArchive.class),
+////                "/", Filters.includeAll());
+//
+//        // Show the deploy structure
+//        _logger.info(war.toString(true));
+//
+//        return war;
+//    }
+//
+//    @Test
+//    public void testServer(@ArquillianResource URL url) throws InterruptedException, URISyntaxException {
+//        // open websocket
+//        URI endpointURI = new URI("ws://" + url.getHost() + ":" + url.getPort() + url.getPath() + "match/m/c");
+//        _logger.info(endpointURI.toString());
+//        final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(endpointURI);
+//
+//        // add listener
+//        clientEndPoint.addMessageHandler(message -> System.out.println(message));
+//
+//        // send message to websocket
+//        clientEndPoint.sendMessage("start match");
+//
+//        // wait 5 seconds for messages from websocket
+//        Thread.sleep(1000);
+//
+//    }
+//}
